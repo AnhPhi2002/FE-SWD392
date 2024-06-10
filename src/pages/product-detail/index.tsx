8016801da654564a4d1f36ba588666cc2009531c
huybq532001 <buiquanghuy532001@gmail.com>
2024-06-10 21:12:51 +0700

    pushproducdetail

--- src/pages/product-detail/index.tsx
+++ src/pages/product-detail/index.tsx
@@ -1,18 +1,22 @@
 import Breadcrumb from "./components/Breadcrumb";
 import Product from "./components/Product";
 import Tabs from "./components/Tabs";
+import Similar from "./components/Similar";
 
 export default function ProductDetail() {
   return (
     <>
-      <section className="pl-40 pr-40 bg-gray-100 ">
+      <section className="pl-40 pr-40 bg- ">
         <Breadcrumb />
       </section>
       <section className="pl-40 pr-40 bg-gray-100 ">
         <Product />
       </section>
-      <section className="pl-40 pr-40 bg-gray-100 ">
+      <section className="pl-40 pr-40">
         <Tabs />
+      </section>
+      <section className="pl-40 pr-40 bg-gray-100 ">
+        <Similar />
       </section>
     </>
   )
